c1c61af6068d1968d5a3f5eb47076bdc94e6c224
reznuhasan <reznuhasan31@gmail.com>
2023-10-09 04:45:25 +0600

    first day

---
+++ AddTwoNumber/AddTwoNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter your second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("total sum result={0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Take Input From User/Take Input From User/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_Input_From_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("My Name is "+name);
+            Console.ReadLine();
+        }
+    }
+}
