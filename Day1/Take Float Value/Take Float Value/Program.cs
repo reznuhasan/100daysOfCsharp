211698ee2eea2fd8e659c3734a3299e6232ac901
reznuhasan <reznuhasan31@gmail.com>
2023-10-09 19:48:30 +0600

    add float taking input

---
+++ Day1/Take Float Value/Take Float Value/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_Float_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
