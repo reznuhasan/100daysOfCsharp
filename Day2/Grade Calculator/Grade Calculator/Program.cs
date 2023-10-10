683f421d4bdf34d708026adb7a7064a0e26f85f3
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 20:27:50 +0600

    grade calculator add

---
+++ Day2/Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Marks:");
+            uint mark = uint.Parse(Console.ReadLine());
+            if (mark>=90 && mark <= 100)
+            {
+                Console.WriteLine("Your Grade is A");
+            }
+            else if(mark>=80 && mark <= 89)
+            {
+                Console.WriteLine("Your Grade is B");
+            }
+            else if(mark>=70 && mark <= 79)
+            {
+                Console.WriteLine("Your Grade is C");
+            }
+            else if(mark>=60 && mark <= 69)
+            {
+                Console.WriteLine("Your Grade is D");
+            }
+            else
+            {
+                Console.WriteLine("Your are fail");
+            }
+            Console.ReadLine();
+        }
+    }
+}
