bcbc99e1e21bd10472ec87364a947150cbc5c588
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 18:00:23 +0600

    add more specifier about date time format and practice

--- Day2/Practice Time Format/Practice Time Format/Program.cs
+++ Day2/Practice Time Format/Practice Time Format/Program.cs
@@ -4,25 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Date_and_Time_Format
+namespace Practice_Time_Format
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            DateTime dt = DateTime.Now;
-           //general situation 
+            DateTime dt= DateTime.Now;
+            //normal date format
             Console.WriteLine("{0}",dt);
             Console.WriteLine("{0:d}",dt);
             Console.WriteLine("{0:D}", dt);
-            Console.WriteLine("{0:f}", dt);
-            Console.WriteLine("{0:F}", dt);
-            Console.WriteLine("{0:g}", dt);
-            Console.WriteLine("{0:G}", dt);
-            Console.WriteLine("{0:t}",dt);
-            Console.WriteLine("{0:T}", dt);
-            Console.WriteLine("{0:m}", dt);
+            Console.WriteLine("{0:f}",dt);
+            Console.WriteLine("{0:F}",dt);
+            Console.WriteLine("{0:G}",dt);
+            Console.WriteLine("{0:g}",dt);
+            Console.WriteLine("{0:m}",dt);
             Console.WriteLine("{0:M}",dt);
+            Console.WriteLine("{0:t}",dt);
+            Console.WriteLine("{0:T}",dt);
             Console.WriteLine("{0:y}",dt);
             Console.ReadLine();
         }
