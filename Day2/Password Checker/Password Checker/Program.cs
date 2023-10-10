23aa1ce180bc58bf48f94205856a26698bcb6cff
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 22:30:53 +0600

    password checker add

---
+++ Day2/Password Checker/Password Checker/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your password:");
+            string password = Console.ReadLine();
+            if (password.Length <= 8)
+            {
+                Console.WriteLine("Password must be more than 8 character");
+            }
+            else
+            {
+                int upper = 0;
+                int lower = 0;
+                int num = 0;
+                int special = 0;
+                foreach (char letter in password)
+                {
+                    if(letter>=65 && letter <= 90)
+                    {
+                        upper++;
+                    }
+                    if(letter>=97 && letter <= 122)
+                    {
+                        lower++;
+                    }
+                    if(letter>=49 && letter <= 58)
+                    {
+                        num++;
+                    }
+                    if((letter>=32 && letter<=47) || (letter >= 58 && letter <= 64) || (letter >= 91 && letter <= 96) || (letter >= 123 && letter <= 126))
+                    {
+                        special++;
+                    }
+                }
+                if (upper == 0)
+                {
+                    Console.WriteLine("Use a upper lettr (A-Z)");
+                }
+                else if (lower == 0)
+                {
+                    Console.WriteLine("Use a lower letter (a-z)");
+                }
+                else if (num == 0)
+                {
+                    Console.WriteLine("Use a number (0-9)");
+                }else if(special == 0)
+                {
+                    Console.WriteLine("Use a special character");
+                }
+                else
+                {
+                    Console.WriteLine("Password create a successfully");
+                    if (password.Length > 12)
+                    {
+                        Console.WriteLine("Quality:- Good");
+                    }else if (special > 3)
+                    {
+                        Console.WriteLine("Quality:- Good");
+                    }else if(special>3 && password.Length > 12)
+                    {
+                        Console.WriteLine("Quality:- Excellent");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Quality:- Average");
+                    }
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
