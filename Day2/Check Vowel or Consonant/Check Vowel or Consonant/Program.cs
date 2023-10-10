bf7b7d0aada9bae073fa3e700bb755169e93ea4e
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 20:50:57 +0600

    check vowel or consonant

---
+++ Day2/Check Vowel or Consonant/Check Vowel or Consonant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Vowel_or_Consonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your letter:");
+            char letter = Console.ReadLine()[0];
+            if(letter=='a' || letter=='e' || letter=='i' || letter=='o' || letter == 'u')
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+            Console.ReadLine();
+        }
+    }
+}
