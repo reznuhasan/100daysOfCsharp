01053d24cff75e690df0060e603894a2cf326b3b
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 17:33:31 +0600

    add time format

---
+++ Day2/Date and Time Format/Date and Time Format/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_and_Time_Format
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+           //general situation 
+            Console.WriteLine("{0}",dt);
+            Console.WriteLine("{0:d}",dt);
+            Console.WriteLine("{0:D}", dt);
+            Console.WriteLine("{0:f}", dt);
+            Console.WriteLine("{0:F}", dt);
+            Console.WriteLine("{0:g}", dt);
+            Console.ReadLine();
+        }
+    }
+}
