e254defc5a18d6d001f4f8a736671d04a30f0af5
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 20:38:53 +0600

    add leap year check program

---
+++ Day2/Leap  Year Check/Leap  Year Check/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap__Year_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your year:");
+            uint year = uint.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine("Leap Year");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Leap Year");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not Leap Year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
