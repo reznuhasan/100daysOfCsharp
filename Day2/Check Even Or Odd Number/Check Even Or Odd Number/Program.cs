7304fc8203ba4ca14224e3963635c7d71abd453c
reznuhasan <reznuhasan31@gmail.com>
2023-10-10 20:19:53 +0600

    add check even or odd number program

---
+++ Day2/Check Even Or Odd Number/Check Even Or Odd Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Even_Or_Odd_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
