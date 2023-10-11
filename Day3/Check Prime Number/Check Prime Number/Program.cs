cddf9ffd48a9880cb2d8eed4eda9c6739db5a09a
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 01:27:30 +0600

    check prime number

---
+++ Day3/Check Prime Number/Check Prime Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Number:");
+            int n = int.Parse(Console.ReadLine());
+            
+            int flag = 0;
+            for(int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag++;
+                }
+            }
+            if (flag > 0)
+            {
+                Console.WriteLine("Not Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
