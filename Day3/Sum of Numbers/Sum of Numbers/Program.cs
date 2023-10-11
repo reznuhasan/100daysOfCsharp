5c6e120f0600dc161124a9441a989b0cde89ef70
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 00:50:27 +0600

    sum of array

---
+++ Day3/Sum of Numbers/Sum of Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your array size:");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+                sum += arr[i];
+            }
+            Console.WriteLine("Result={0}",sum);
+            Console.ReadLine();
+        }
+    }
+}
