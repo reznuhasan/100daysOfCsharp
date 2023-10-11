9bbe364370d9db3fe2312ba55156975959d3390c
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 01:06:46 +0600

    factorial calculation

---
+++ Day3/Factorial Calculation/Factorial Calculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Factorial Number:");
+            int n = int.Parse(Console.ReadLine());
+            int fac=1;
+            for(int i = 1; i <=n; i++)
+            {
+                fac *= i;
+            }
+
+            Console.WriteLine(fac);
+            Console.ReadLine();
+
+        }
+    }
+}
