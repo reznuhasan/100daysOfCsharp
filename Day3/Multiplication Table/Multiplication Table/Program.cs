88ce4b4b34e7b914170879df23d115e232fff6f0
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 01:19:43 +0600

    Multiplication table create

---
+++ Day3/Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your multiplication number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0}  *  {1} = {2}",i,n,i*n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
