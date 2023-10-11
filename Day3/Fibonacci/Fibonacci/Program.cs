021e0c8ed1db47118726fc0c974929a852c67358
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 01:36:52 +0600

    make fibonacci series

---
+++ Day3/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number:");
+            int n = int.Parse(Console.ReadLine());
+            int a = 1, b = -1;
+            for(int i = 0; i < n; i++)
+            {
+                int sum = a + b;
+                Console.Write("{0} ",sum);
+                b = a;
+                a = sum;
+            }
+            Console.ReadLine();
+        }
+    }
+}
