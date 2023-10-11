6ccd34bd5b11ceeba3af2641ba08e8fc056a9eb1
reznuhasan <reznuhasan31@gmail.com>
2023-10-12 01:51:19 +0600

    check palindrome string

---
+++ Day3/Check Palindrome In String/Check Palindrome In String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Palindrome_In_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word:");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            int count = 0;
+            for(int i = 0, j = word.Length - 1; i < j; i++, j--)
+            {
+                if (word[i]!= word[j])
+                {
+                    count++;
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
