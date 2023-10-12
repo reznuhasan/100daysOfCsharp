dfeec98c022e4359947072f1d167c37dc5c237d1
reznuhasan <reznuhasan31@gmail.com>
2023-10-13 05:20:43 +0600

    add 282A and 263A problem solution

---
+++ Day4/Problem 263A/Problem 263A/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_263A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat = new int[5, 5];
+            for(int i = 0; i < 5; i++)
+            {
+                string[] userInput = Console.ReadLine().Split(' ');
+                for(int j = 0; j < 5; j++)
+                {
+                    if (userInput.Length > j)
+                    {
+                        mat[i,j]=int.Parse(userInput[j]); 
+                    }
+                }  
+            }
+            int col=0,row=0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (mat[i, j] == 1)
+                    {
+                        col = j+1;
+                        row = i+1;
+                    }
+                }
+            }
+            int count = 0;
+            if (row > 3)
+            {
+                count += (row - 3);
+            }
+            else if (row < 3){
+                count += (3 - row);
+            }
+            if (col > 3)
+            {
+                count += (col - 3);
+            }
+            else if (col < 3){
+                count += (3 - col);
+            }
+            Console.WriteLine("{0}",count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4/Problem 282A/Problem 282A/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_282A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < t; i++)
+            {
+                string word="";
+                string operators = Console.ReadLine();
+                if(operators=="++X" || operators=="X++" || operators=="--X" || operators == "X--")
+                {
+                    word += operators;
+                }
+                if(word=="++X" || word == "X++")
+                {
+                    count++;
+                }else if(word=="--X" || word == "X--")
+                {
+                    count--;
+                }
+            }
+            Console.WriteLine("{0}",count);
+            Console.ReadLine();
+        }
+    }
+}
