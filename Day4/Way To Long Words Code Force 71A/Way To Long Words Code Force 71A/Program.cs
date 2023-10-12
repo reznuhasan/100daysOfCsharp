f106c74e807b3c980e194bf4862c94ca4586337a
reznuhasan <reznuhasan31@gmail.com>
2023-10-13 03:03:57 +0600

    problem 71A add successfully

---
+++ Day4/Way To Long Words Code Force 71A/Way To Long Words Code Force 71A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Way_To_Long_Words_Code_Force_71A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for(int i = 0; i < t; i++)
+            {
+                string word = Console.ReadLine();
+                if (word.Length<=10)
+                {
+                    Console.WriteLine(word);
+                }
+                else
+                {
+                    int count = 0;
+                    for(int j = 1; j < word.Length-1; j++)
+                    {
+                        count++;
+                    }
+                    string newWord = word[0] + Convert.ToString(count) + word[word.Length - 1];
+                    Console.WriteLine(newWord);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
