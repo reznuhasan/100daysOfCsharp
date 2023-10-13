f50fd3bf3eee447a43e8c569e77ee5faf4be004b
reznuhasan <reznuhasan31@gmail.com>
2023-10-13 06:30:11 +0600

    add 112A and 118A problem solution

---
+++ Day4/Problem 112A/Problem 112A/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_112A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string take1 = Console.ReadLine();
+            string take2 = Console.ReadLine();
+            string word1 = "";
+            string word2 = "";
+            if (take1.Length == take2.Length)
+            {
+                word1 = take1;
+                word2 = take2;
+            }
+            word1 = word1.ToLower();
+            word2 = word2.ToLower();
+            int value1 = 0;
+            int value2 = 0;
+            for(int i = 0; i < word2.Length; i++)
+            {
+                if (word1[i] != word2[i])
+                {
+                    value1 = Convert.ToInt32(word1[i]);
+                    value2 = Convert.ToInt32(word2[i]);
+                    break;
+                }
+            }
+            if (value1 == value2)
+            {
+                Console.WriteLine(0);
+            }else if (value1 > value2)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4/Problem 118A/Problem 118A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_118A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleInput = Console.ReadLine();
+            int count = 0;
+            for(int i = 0; i < sampleInput.Length; i++)
+            {
+                if ((Convert.ToInt32(sampleInput[i])>=65 && Convert.ToInt32(sampleInput[i])<=90) || (Convert.ToInt32(sampleInput[i]) >= 97 && Convert.ToInt32(sampleInput[i]) <= 122))
+                {
+                    count++;
+                }
+            }
+            string word = "";
+            if (sampleInput.Length == count)
+            {
+                word = sampleInput.ToLower();
+            }
+            string sampleResult = "";
+            for(int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != 'a' && word[i] != 'e' && word[i] != 'o' && word[i] != 'i' && word[i] !='y' && word[i] != 'u')
+                {
+                    sampleResult += ("." + word[i]);
+                }
+            }
+            Console.WriteLine("{0}",sampleResult);
+            Console.ReadLine();
+        }
+    }
+}
