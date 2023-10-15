8042873b8d6d91bd786ec8377a880c75258d7220
reznuhasan <reznuhasan31@gmail.com>
2023-10-16 04:02:42 +0600

    add problem 133A solution

---
+++ Day6/Problem 133A/Problem 133A/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_133A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string p;
+            while (true)
+            {
+                p = Console.ReadLine();
+                if(p.Length>=1 && p.Length <= 100)
+                {
+                    break;
+                }
+            }
+            int count = 0;
+            for(int i = 0; i < p.Length; i++)
+            {
+                if (p[i] == 'H')
+                {
+                    count++;
+                    break;
+                }else if (p[i] == 'Q')
+                {
+                    count++;
+                    break;
+                }else if (p[i] == '9')
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            Console.ReadLine();
+        }
+    }
+}
