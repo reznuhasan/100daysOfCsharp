446cd98996d388ec0e187232145b8f4d00f45f6e
reznuhasan <reznuhasan31@gmail.com>
2023-10-15 13:49:00 +0600

    add problem 617A & 59A solution

---
+++ Day5/Problem 59A/Problem 59A/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_59A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            while (true)
+            {
+                word = Console.ReadLine();
+                if(word.Length>=1 && word.Length <= 100)
+                {
+                    break;
+                }
+            }
+            int lower = 0;
+            int upper = 0;
+            for(int i = 0; i < word.Length; i++)
+            {
+                int num = Convert.ToInt32(word[i]);
+                if(num>=97 && num <= 122)
+                {
+                    lower++;
+                }else if(num>=65 && num <= 90)
+                {
+                    upper++;
+                }
+            }
+            if (upper >lower)
+            {
+                Console.WriteLine("{0}",word.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine("{0}", word.ToLower());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day5/Problem 617A/Problem 617A/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_617A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint x;
+            while (true)
+            {
+                x = uint.Parse(Console.ReadLine());
+                if(x>=1 && x <= 1000000)
+                {
+                    break;
+                }
+            }
+            int count = 0;
+            while (true)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                if (x >= 5)
+                {
+                    x -= 5;
+                }else if(x >= 4)
+                {
+                    x -= 4;
+                }else if (x >= 3)
+                {
+                    x -= 3;
+                }
+                else if (x >= 2)
+                {
+                    x -= 2;
+                }
+                else if (x >= 1)
+                {
+                    x -= 1;
+                }
+                count++;
+            }
+            Console.WriteLine("{0}",count);
+            Console.ReadLine();
+            
+        }
+    }
+}
