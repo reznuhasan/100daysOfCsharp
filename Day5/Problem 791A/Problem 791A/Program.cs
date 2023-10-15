121684e3421f6af239ca625b036d12f76c111d7e
reznuhasan <reznuhasan31@gmail.com>
2023-10-15 13:02:48 +0600

    problem 977A solution

---
+++ Day5/Problem 791A/Problem 791A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_791A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint limak;
+            uint bob;
+            while (true)
+            {
+                string numbers=Console.ReadLine();
+                string[] numbersArr = numbers.Split(' ');
+                limak = uint.Parse(numbersArr[0]);
+                bob = uint.Parse(numbersArr[1]);
+                if((limak>=1 && limak<=10) && (bob>=1 && bob <= 10))
+                {
+                    break;
+                }
+            }
+            uint count=0;
+            while (true)
+            {
+                limak *= 3;
+                bob *= 2;
+                count++;
+                if (limak > bob)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("{0}",count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day5/Problem 977A/Problem 977A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_977A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number, time;
+            while (true)
+            {
+                string sampleInput = Console.ReadLine();
+                string[] inputs = sampleInput.Split(' ');
+                number = uint.Parse(inputs[0]);
+                time = uint.Parse(inputs[1]);
+                if((number>=2 && number<= 1000000000) && (time>=1 && time <= 50))
+                {
+                    break;
+                }
+            }
+            for(uint i = time; i > 0; i--)
+            {
+                if (number % 10 == 0)
+                {
+                    number /= 10;
+                }
+                else
+                {
+                    number--;
+                }
+            }
+            Console.WriteLine("{0}",number);
+            Console.ReadLine();
+
+        }
+    }
+}
