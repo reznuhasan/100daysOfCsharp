2f49c7603cf435dea2a919947ce133a326df80cf
reznuhasan <reznuhasan31@gmail.com>
2023-10-21 04:02:17 +0600

    add problem 467A solution

---
+++ Day7/Problem 467A/Problem 467A/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_467A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n>=1 && n <= 100)
+                {
+                    break;
+                }
+            }
+            int room = 0;
+            int availableRoom = 0;
+            while (true)
+            {
+                string sample = Console.ReadLine();
+                string[] inputs = sample.Split(' ');
+                int p = int.Parse(inputs[0]);
+                int q = int.Parse(inputs[1]);
+                if((p>=0 && p<=100) && (q>=0 && q<=100) && p <= q)
+                {
+                    if (q-p>=2)
+                    {
+                        availableRoom++;
+                    }
+                    room++;
+                }
+                if (room == n)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("{0}",availableRoom);
+            Console.ReadLine();
+
+        }
+    }
+}
