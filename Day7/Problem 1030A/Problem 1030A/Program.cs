bfa0c7c557765e0da6f789944181be17e77b590a
reznuhasan <reznuhasan31@gmail.com>
2023-10-21 03:37:48 +0600

    add Problem 1030A solution

---
+++ Day7/Problem 1030A/Problem 1030A/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1030A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n>=1 && n <= 100)
+                {
+                    break;
+                }
+            }
+            string[] inputs;
+            while (true)
+            {
+                string sampleInput = Console.ReadLine();
+                inputs = sampleInput.Split(' ');
+                int flag= 0;
+                foreach(string num in inputs)
+                {
+                    if(num=="1" || num == "0")
+                    {
+                        flag++;
+                    }
+                }
+                if (flag == inputs.Length && flag==n)
+                {
+                    break;
+                }
+            }
+            int hard = 0;
+            foreach(string ch in inputs)
+            {
+                if (ch == "1")
+                {
+                    hard = 1;
+                    break;
+                }
+            }
+            if (hard == 1)
+            {
+                Console.WriteLine("HARD");
+            }
+            else
+            {
+                Console.WriteLine("EASY");
+            }
+            Console.ReadLine();
+        }
+    }
+}
