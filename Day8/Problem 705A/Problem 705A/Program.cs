899c6fd07211fb9c0438a3143717ab05f864d4d6
reznuhasan <reznuhasan31@gmail.com>
2023-10-23 02:14:07 +0600

    add problem 705A solution

---
+++ Day8/Problem 705A/Problem 705A/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_705A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n>=1 && n <= 100)
+                {
+                    break;
+                }
+            }
+            string str = "";
+            for(int i = 1; i <n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    str += "I hate that ";
+                }
+                else
+                {
+                    str += "I love that ";
+                }
+            }
+            if (n % 2 == 1)
+            {
+                str += "I hate it";
+            }
+            else
+            {
+                str += "I love it";
+            }
+            Console.WriteLine("{0}",str);
+            Console.ReadLine();
+        }
+    }
+}
