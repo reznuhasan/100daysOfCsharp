cd467d954f0ef1553f80cb18fe00a4ed73b874a8
reznuhasan <reznuhasan31@gmail.com>
2023-10-24 03:53:05 +0600

    add constructor chaining

---
+++ Day8/Constructor Chaining/Constructor Chaining/Person.cs
@@ -0,0 +1,12 @@
+﻿
+class Person
+{
+    public string fname;
+    public string lname;
+    public Person() : this(string.Empty, string.Empty) { }
+    public Person(string fname,string lname)
+    {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
