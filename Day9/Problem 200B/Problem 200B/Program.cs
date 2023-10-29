90f693713e9cfee9025fe6ace7fde08f6b11cd99
reznuhasan <reznuhasan31@gmail.com>
2023-10-29 21:07:59 +0600

    add solution 200B

---
+++ Day9/Problem 200B/Problem 200B/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_200B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n>=1 && n <= 100)
+                {
+                    break;
+                }
+            }
+            int[] arr = new int[n];
+
+            while (true) {
+                string sample = Console.ReadLine();
+                string[] inputs =sample.Split(' ');
+                int flag = 0;
+                foreach (string ch in inputs)
+                {
+                    int value = int.Parse(ch);
+                    if(value>=0 && value <= 100)
+                    {
+                        arr[flag] = value;
+                        flag++;
+                    }
+                }
+                if (flag == n)
+                {
+                    break;
+                }
+            }
+            int sum = 0;
+            foreach(int num in arr)
+            {
+                sum += num;
+            }
+            double result = (double)sum / (double)n;
+            string formatValue = result.ToString("F12");
+            Console.WriteLine("{0}",formatValue);
+            Console.ReadLine();
+        }
+    }
+}
