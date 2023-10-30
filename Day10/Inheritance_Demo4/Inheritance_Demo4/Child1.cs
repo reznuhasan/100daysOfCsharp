eb8a389a1b25e75aab27d08cf197a098f018679c
reznuhasan <reznuhasan31@gmail.com>
2023-10-31 03:02:39 +0600

    add inheritance practice

---
+++ Day10/Inheritance_Demo4/Inheritance_Demo4/Child1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Demo4
+{
+    internal class Child1:Program
+    {
+        public Child1(int i) : base(i)
+        {
+            Console.WriteLine("This is child constructor");
+        }
+        static void Main()
+        {
+            Console.Write("Enter your number: ");
+            int num = int.Parse(Console.ReadLine());
+            Child1 obj = new Child1(num);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day10/Inheritance_Demo4/Inheritance_Demo4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Demo4
+{
+    internal class Program
+    {
+        public Program(int i)
+        {
+            Console.WriteLine("My value is {0}",i);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
