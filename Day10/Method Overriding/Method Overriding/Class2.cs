4e8da2020213e2656dee2f612243b624f67a1a87
reznuhasan <reznuhasan31@gmail.com>
2023-10-31 03:43:18 +0600

    add Method Overriding practice

---
+++ Day10/Method Overriding/Method Overriding/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    internal class Class2:Program
+    {
+        public override void greeting()
+        {
+            Console.WriteLine("Rizwan is a good boy");
+        }
+        static void Main()
+        {
+            Class2 obj = new Class2();
+            obj.greeting();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day10/Method Overriding/Method Overriding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    internal class Program
+    {
+        public virtual void greeting()
+        {
+            Console.WriteLine("I'm a good boy");
+        }
+    }
+}
