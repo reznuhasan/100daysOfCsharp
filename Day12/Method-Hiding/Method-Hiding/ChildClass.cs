6c5b0aa0f98176beb02b8ba6cd1f64e1d58bcddf
reznuhasan <reznuhasan31@gmail.com>
2023-11-22 04:13:35 +0600

    practice method hiding

---
+++ Day12/Method-Hiding/Method-Hiding/ChildClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+    public class ChildClass:Program
+    {
+        public ChildClass() : base()
+        {
+
+        }
+        public override void test()
+        {
+            Console.WriteLine("This is child Method");
+        }
+        public new void test1()
+        {
+            Console.WriteLine("This is child Method");
+        }
+        public static void Main(string[] args)
+        {
+            ChildClass c = new ChildClass();
+            c.test1();
+            c.test();
+            Program p = new ChildClass();
+            p.test();
+            p.test1();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day12/Method-Hiding/Method-Hiding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+    public class Program
+    {
+        public string name;
+        public int age;
+      
+        public Program() { }
+        public Program(string name,int age) { 
+            this.name = name;
+            this.age = age;
+        }
+        public void showDetails()
+        {
+            Console.WriteLine("Name:{0},Age {1}",this.name,this.age);
+        }
+        //overriding method
+        public virtual void test()
+        {
+            Console.WriteLine("This is Parent Method");
+        }
+        //method hiding or shadowing
+        public void test1()
+        {
+            Console.WriteLine("This is parent method2");
+        }
+    }
+}
