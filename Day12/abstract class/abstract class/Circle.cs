db7a3ae10426c66e933e776e9f723b740fa34837
reznuhasan <reznuhasan31@gmail.com>
2023-11-23 02:45:04 +0600

    add abstract class and method practice

---
+++ Day12/abstract class/abstract class/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    internal class Circle:Program
+    {
+        public override void Area()
+        {
+            Console.WriteLine("Area is {0}",pi*(radius*radius));
+        }
+        public static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.radius = 3;
+            c.Area();
+            c.test();
+            Console.ReadLine();
+
+        }
+    }
+    
+}
---
+++ Day12/abstract class/abstract class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    public abstract class Program
+    {
+        public int length,width,height,radius;
+        public const double pi= 3.1416;
+        public abstract void Area();
+        public void test()
+        {
+            Console.WriteLine("Program run successfully");
+        }
+    }
+}
