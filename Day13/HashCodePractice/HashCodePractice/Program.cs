b4cb496378ddc6d2e09c33a625e08f4ae1ba455e
reznuhasan <reznuhasan31@gmail.com>
2023-12-07 02:21:35 +0600

    add hashTable practice

---
+++ Day13/HashCodePractice/HashCodePractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashCodePractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Name", "Rizwan Hasan");
+            ht.Add("Age", 25);
+            ht.Add("Single", true);
+            ht.Remove("Age");
+            ht.Clear();
+            ht.Add("Id", "123456");
+            
+            foreach(string key in ht.Keys)
+            {
+                Console.WriteLine(ht[key]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
