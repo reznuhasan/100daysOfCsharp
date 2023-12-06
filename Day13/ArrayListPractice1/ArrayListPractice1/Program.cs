88306032309c0cf59f37f1819fcb403a5ae5741b
reznuhasan <reznuhasan31@gmail.com>
2023-12-07 03:03:16 +0600

    generic list practice

---
+++ Day13/ArrayListPractice1/ArrayListPractice1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace ArrayListPractice1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obj = new ArrayList();
+            obj.Add(30);
+            obj.Add(50);
+            obj.Insert(2, 70);
+            obj.Insert(3, 20);
+            obj.RemoveAt(2);
+            foreach (var item in obj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day13/GenericList/GenericList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GenericList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(70);
+            list.Add(60);
+            list.Add(50);
+            list.Add(40);
+            list.Add(80);
+            list.Add(90);
+            list.Insert(2,100);
+            list.RemoveAt(3);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
