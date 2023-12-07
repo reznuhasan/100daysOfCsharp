67ff51b5727fba82cc22b1f054160b95c5698522
reznuhasan <reznuhasan31@gmail.com>
2023-12-07 07:49:31 +0600

    add store object in generic list

---
+++ Day14/Object In GenericList/Object In GenericList/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_In_GenericList
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Age {  get; set; }
+        public Person(string name, int id, string title, int age)
+        {
+            Name = name;
+            Id = id;
+            Title = title;
+            Age = age;
+        }
+    }
+}
---
+++ Day14/Object In GenericList/Object In GenericList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_In_GenericList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Rizwan",1234,"Student",25);
+            Person person2 = new Person("Asif", 1235, "Student", 25);
+            Person person3 = new Person("Tonoy", 1236, "Student", 25);
+            Person person4 = new Person("Ajad Munshi", 1237, "Teacher", 25);
+
+            List<Person> li = new List<Person>();
+            li.Add(person1);
+            li.Add(person2);
+            li.Add(person3);
+            li.Add(person4);
+
+            foreach (Person p in li)
+            {
+                Console.WriteLine(p.Name + " : " + p.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
