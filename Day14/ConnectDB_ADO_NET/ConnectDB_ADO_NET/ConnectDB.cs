cc8c659c36e43ac45335e474ab351aafea1fda7b
reznuhasan <reznuhasan31@gmail.com>
2023-12-08 04:28:17 +0600

    sql server add successfully

---
+++ Day14/ConnectDB_ADO_NET/ConnectDB_ADO_NET/ConnectDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectDB_ADO_NET
+{
+    public class ConnectDB
+    {
+        static void Main(string[] args)
+        {
+            ConnectDB.Connection();
+            Console.ReadLine();
+        }
+        static void Connection()
+        {
+            string connectionString = "Data Source=DESKTOP-6P8AMML\\SQLEXPRESS;Initial Catalog=Practice;Integrated Security=true";
+            SqlConnection con=new SqlConnection(connectionString);
+            con.Open();
+            if(con.State==System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection has been created Successfully");
+            }
+            con.Close();
+        }
+    }
+    
+}
