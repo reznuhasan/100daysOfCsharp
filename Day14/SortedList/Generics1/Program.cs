b39e590ac062362b1862c345bb273fcf48eecd10
reznuhasan <reznuhasan31@gmail.com>
2023-12-07 08:51:18 +0600

    sorted list

---
+++ Day14/SortedList/Generics1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(10);
+            list.Add(30);
+            list.Add(50);
+            list.Add(20);
+            list.Add(40);
+            list.Sort();
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
