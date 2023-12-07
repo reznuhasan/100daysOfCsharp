13fafe60113c2960368b10af760a601d785fdd73
reznuhasan <reznuhasan31@gmail.com>
2023-12-07 07:59:15 +0600

    add dictionary practice

---
+++ Day14/DictoinaryPractice/DictoinaryPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictoinaryPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("Name", "Rizwan");
+            dict.Add("Age", 25);
+            dict.Add("Single", true);
+            
+            foreach(string key in dict.Keys)
+            {
+                Console.WriteLine(key+" : " + dict[key]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
