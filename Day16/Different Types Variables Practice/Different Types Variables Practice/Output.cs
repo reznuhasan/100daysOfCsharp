c474f28c4ed9cb9bbcb8dda21b0d8688119f78dc
reznuhasan <reznuhasan31@gmail.com>
2024-01-10 04:11:59 +0600

    variable practice

---
+++ Day16/Different Types Variables Practice/Different Types Variables Practice/Output.cs
@@ -0,0 +1,16 @@
+﻿
+
+using DPClass=Different_Types_Variables_Practice.Program;
+using printOutput = System.Console;
+
+DPClass program1 = new DPClass("program1");
+/*Console.WriteLine(DPClass.number);
+DPClass.number = 400;
+Console.WriteLine(DPClass.number);
+*/
+program1.DisplayClassName();
+printOutput.WriteLine(DPClass.number);
+printOutput.WriteLine(DPClass.pi);
+/*gives readonly variables error cause 
+program1.intanceName = "Program2";
+*/
---
+++ Day16/Different Types Variables Practice/Different Types Variables Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Different_Types_Variables_Practice
+{
+    public class Program
+    {
+       public static int number = 300;
+        public const float pi = 3.14f;
+        public readonly string intanceName;
+
+        public Program(string name)
+        {
+            intanceName = name;
+        }
+        public void DisplayClassName()
+        {
+            Console.WriteLine(intanceName);
+        }
+    }
+}
