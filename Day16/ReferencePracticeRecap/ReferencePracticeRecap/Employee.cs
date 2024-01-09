9710c16041593450ced15d2dd4331830400c5b5c
reznuhasan <reznuhasan31@gmail.com>
2024-01-10 03:00:12 +0600

    reference recap

---
+++ Day16/ReferencePracticeRecap/ReferencePracticeRecap/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferencePracticeRecap
+{
+    public class Employee:Person
+    {
+        string Salary { get; set; }
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            obj.Name = "Alif";
+            obj.Salary = "40000";
+            Console.WriteLine("{0} monthly Salary {1}",obj.Name,obj.Salary);
+        }
+
+    }
+}
---
+++ Day16/ReferencePracticeRecap/ReferencePracticeRecap/Person.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ReferencePracticeRecap
+{
+    public class Person
+    {
+       public string Name { get; set; }
+        /*static void Main(string[] args)
+        {
+            Person person1 =new Person();
+            Person person2 = person1;
+            person1.Name = "Rizwan";
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person2.Name);
+            person2.Name = "Alif";
+            Console.WriteLine(person2.Name);
+            Console.WriteLine(person1.Name);
+        }*/
+    }
+}
