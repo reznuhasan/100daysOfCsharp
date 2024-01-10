cbaf5917ea238eeb37c3fbb3342888108ecf525c
reznuhasan <reznuhasan31@gmail.com>
2024-01-11 02:01:28 +0600

    constructor Chaining practice

---
+++ Day16/ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ConstructorChaining
+{
+    public class Program
+    {
+        public string name;
+        public string description;
+        public Program(string name):this(name,"Alif Layla"){ 
+            this.name = name;
+            Console.WriteLine("One Paramiterlaized Constructor");
+        }
+        public Program(string name,string description)
+        {
+            this.name = name;
+            this.description = description;
+            Console.WriteLine("Two Paramiterlaized Constructor");
+        }
+        /*public Program():this("Hossen","He is a playboy")
+        {
+            Console.WriteLine("Paramiterless Constructor");
+        }*/
+        public void Display()
+        {
+            Console.WriteLine("Name={0},des={1}", this.name, this.description);
+        }
+        static void Main(string[] args)
+        {
+           /* Program obj = new Program();
+            obj.Display();*/
+        }
+    }
+}
---
+++ Day16/ConstructorChaining/ConstructorChaining/Program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    public class Program2:Program
+    {
+        public int age;
+        public Program2(int age):base("Ali","Good Student")
+        {
+            this.age = age;
+        }
+       static void Main(string[] args)
+        {
+            Program2 obj = new Program2(20);
+            Console.WriteLine("age={0},name={1},des={2}",obj.age,obj.name,obj.description);
+        }
+    }
+}
