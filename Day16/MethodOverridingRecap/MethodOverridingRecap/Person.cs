6aaecaa09b08d6c1edabec28cbde47897d59ee90
reznuhasan <reznuhasan31@gmail.com>
2024-01-12 18:23:47 +0600

    method overriding and hiding recap

---
+++ Day16/MethodOverridingRecap/MethodOverridingRecap/Person.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MethodOverridingRecap
+{
+    public class Person
+    {
+       public virtual void Display() {
+            Console.WriteLine("Parent Method");
+        }
+        public void Display1()
+        {
+            Console.WriteLine("Parent Hiding");
+        }
+    }
+}
---
+++ Day16/MethodOverridingRecap/MethodOverridingRecap/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverridingRecap
+{
+    public class Student:Person
+    {
+        public override void Display() {
+            Console.WriteLine("Child Method");
+        }
+        public new void Display1()
+        {
+            Console.WriteLine("Child Hiding");
+        }
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Display();
+            s.Display1();
+            Person p=new Student();
+            p.Display();
+            p.Display1();
+        }
+    }
+}
