fc51e74dd0094d749d497fcb9b9f6c3ad076fcdd
reznuhasan <reznuhasan31@gmail.com>
2024-01-15 02:08:10 +0600

    abstract recap

---
+++ Day17/AbstractRecap/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AbstractRecap
+{
+    public abstract class Person
+    {
+        public abstract void ShowName(string name);
+    }
+    public class Employee:Person
+    {
+        public override void ShowName(string name)
+        {
+            Console.WriteLine(name);
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.ShowName("Rizwan");
+        }
+    }
+ 
+}
