123958b29ad4b7295762d1992ab5915b343d8176
reznuhasan <reznuhasan31@gmail.com>
2024-01-18 01:05:26 +0600

    Matrix Subtraction

---
+++ Day17/MatrixSub/MatrixSub/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MatrixSub
+{
+    public class Program
+    {
+        int a, b,c,d;
+        public Program(int a,int b,int c,int d)
+        {
+            this.a = a; this.b = b; 
+            this.c = c; this.d = d;
+        }
+        public override string ToString()
+        {
+            return "a = "+this.a+" b = "+this.b+" c = "+this.c+" d = "+this.d;
+        }
+        public static Program operator +(Program obj1, Program obj2)
+        {
+            Program obj = new Program(obj1.a - obj2.a, obj1.b - obj2.b, obj1.c - obj2.c, obj1.d - obj2.d);
+            return obj;
+        }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program(43,23,33,53);
+            Program obj2 = new Program(10, 20, 30, 40);
+            Program obj=obj1 + obj2;
+            Console.WriteLine(obj);
+
+        }
+    }
+}
--- Day17/OperatorOverloadingPractice/OperatorOverloadingPractice/obj/Debug/net8.0/OperatorOverloadingPractice.AssemblyInfo.cs
+++ Day17/OperatorOverloadingPractice/OperatorOverloadingPractice/obj/Debug/net8.0/OperatorOverloadingPractice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorOverloadingPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f64a042f8adee74d3dd4399bab2163c0c9fcc28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorOverloadingPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorOverloadingPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
