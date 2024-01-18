88f234dc2b14accf8dc531e2b8f66193d88c947e
reznuhasan <reznuhasan31@gmail.com>
2024-01-18 11:09:35 +0600

    Delegate Practice 2 add

---
+++ Day17/DelegatePractice2/DelegatePractice2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace DelegatePractice2
+{
+    public delegate void GreetingsDelegate(string name);
+    public delegate string FindNameDelegate(string name);
+    public delegate int OperationDelegate(int a,int b);
+    public class Program
+    {
+        public static void Greetings(String name)
+        {
+            Console.WriteLine("Hello " + name + ", Good Morning");
+        }
+        static void Main(string[] args)
+        {
+            GreetingsDelegate obj = Program.Greetings;
+            obj("Rizwan");
+            FindNameDelegate findName = delegate (string name)
+            {
+                return name;
+            };
+            string Name=findName("Leo");
+            //annonymus delegate
+            OperationDelegate add = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            //lamda delegate
+            OperationDelegate sub = (int c, int d) =>
+            {
+                return c - d;
+            };
+            Console.WriteLine(add(45, 69));
+            Console.WriteLine(sub(20, 12));
+            Console.WriteLine(Name);
+
+        }
+    }
+}
