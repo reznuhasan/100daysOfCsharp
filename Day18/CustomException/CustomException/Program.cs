4e220379b9eac51ba03e330d8bb8dc49a4c9bcd7
reznuhasan <reznuhasan31@gmail.com>
2024-01-30 13:42:39 +0600

    custom exception are add

---
+++ Day18/CustomException/CustomException/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    if (b % 2 == 1)
+    {
+        throw new ApplicationException("odd number not allowed");
+    }
+    int c = a / b;
+    Console.WriteLine("the result is {0}", c);
+}catch(FormatException ex1)
+{
+    Console.WriteLine("Only Numric value are allowed");
+}catch(DivideByZeroException ex2)
+{
+    Console.WriteLine("Divided by Zero is not possible");
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Division Part are finished");
+}
+Console.WriteLine("Program are End");
--- Day18/ExceptionHandaling/ExceptionHandaling/obj/Release/net8.0/ExceptionHandaling.AssemblyInfo.cs
+++ Day18/ExceptionHandaling/ExceptionHandaling/obj/Release/net8.0/ExceptionHandaling.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionHandaling")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba17a26cde1e01efb4b068a26ff505f50e0e3b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionHandaling")]
